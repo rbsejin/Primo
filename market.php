4234d2cedfd7aa02d23ffd0f47507274c4c66abb
rbsejin <rbsejin@gmail.com>
2021-04-01 17:54:27 +0900

    마켓 화면 추가

--- market.php
+++ market.php
@@ -3,8 +3,118 @@
 <?php
 include_once('header.php');
 ?>
-    
-</body>
+
+<main>
+    <!-- Hero Area Start-->
+    <div class="slider-area ">
+        <div class="single-slider slider-height2 d-flex align-items-center">
+            <div class="container">
+                <div class="row">
+                    <div class="col-xl-12">
+                        <div class="hero-cap text-center">
+                            <h2>마켓</h2>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Hero Area End-->
+
+    <section class="popular-items latest-padding">
+        <div class="container">
+            <div class="row product-btn justify-content-between mb-40">
+                <div class="properties__button">
+                    <!--Nav Button  -->
+                    <nav>
+                        <div class="nav nav-tabs" id="nav-tab" role="tablist">
+                            <a class="nav-item nav-link" id="nav-contact-tab" data-toggle="tab" href="#nav-contact" role="tab" aria-controls="nav-contact" aria-selected="false"> 운정중 </a>
+                            <a class="nav-item nav-link active" id="nav-home-tab" data-toggle="tab" href="#nav-home" role="tab" aria-controls="nav-home" aria-selected="true"> 한빛중 </a>
+                            <a class="nav-item nav-link" id="nav-profile-tab" data-toggle="tab" href="#nav-profile" role="tab" aria-controls="nav-profile" aria-selected="false"> 한빛고 </a>
+                            <a class="nav-item nav-link" id="nav-contact-tab" data-toggle="tab" href="#nav-contact" role="tab" aria-controls="nav-contact" aria-selected="false"> 동패고 </a>
+                        </div>
+                    </nav>
+                    <!--End Nav Button  -->
+                </div>
+                <!-- Grid and List view -->
+                <div class="grid-list-view">
+                </div>
+                <!-- Select items -->
+                <!-- <div class="select-this">
+                    <form action="#">
+                        <div class="select-itms">
+                            <select name="select" id="select1">
+                                <option value="">40 per page</option>
+                                <option value="">50 per page</option>
+                                <option value="">60 per page</option>
+                                <option value="">70 per page</option>
+                            </select>
+                        </div>
+                    </form>
+                </div> -->
+            </div>
+
+            <div class="tab-content" id="nav-tabContent">
+                <!-- card one -->
+                <div class="tab-pane fade show active" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab">
+                    <div class="row">
+                        <div class="col-xl-4 col-lg-4 col-md-6 col-sm-6">
+                            <div class="single-popular-items mb-50 text-center">
+                                <div class="popular-img">
+                                    <img src="assets/img/gallery/market/ujm_w_jacket.jpg" alt="">
+                                    <div class="img-cap">
+                                        <span>장바구니에 추가</span>
+                                    </div>
+                                    <div class="favorit-items">
+                                        <span class="flaticon-heart"></span>
+                                    </div>
+                                </div>
+                                <div class="popular-caption">
+                                    <h3><a href="product_details.html">Thermo Ball Etip Gloves</a></h3>
+                                    <span>$ 45,743</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-xl-4 col-lg-4 col-md-6 col-sm-6">
+                            <div class="single-popular-items mb-50 text-center">
+                                <div class="popular-img">
+                                <img src="assets/img/gallery/market/ujm_w_blouse.jpg" alt="">
+                                    <div class="img-cap">
+                                        <span>장바구니에 추가</span>
+                                    </div>
+                                    <div class="favorit-items">
+                                        <span class="flaticon-heart"></span>
+                                    </div>
+                                </div>
+                                <div class="popular-caption">
+                                    <h3><a href="product_details.html">Thermo Ball Etip Gloves</a></h3>
+                                    <span>$ 45,743</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-xl-4 col-lg-4 col-md-6 col-sm-6">
+                            <div class="single-popular-items mb-50 text-center">
+                                <div class="popular-img">
+                                <img src="assets/img/gallery/market/ujm_c_hood.jpg" alt="">
+                                    <div class="img-cap">
+                                        <span>장바구니에 추가</span>
+                                    </div>
+                                    <div class="favorit-items">
+                                        <span class="flaticon-heart"></span>
+                                    </div>
+                                </div>
+                                <div class="popular-caption">
+                                    <h3><a href="product_details.html">Thermo Ball Etip Gloves</a></h3>
+                                    <span>$ 45,743</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</main>
 
 <?php
 include_once('footer.php');
