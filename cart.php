f0c9695d6b1d34967fb24b97f3461449ec5b73b1
rbsejin <rbsejin@gmail.com>
2021-04-08 17:33:43 +0900

    cart에서 반복되는 html구문을 php문 밖으로 뺌

--- cart.php
+++ cart.php
@@ -63,7 +63,6 @@ $result = mysqli_query($conn, $sql);
             </thead>
             <tbody>
               <?php
-              $list = '';
               $totalPrice = 0;
 
               while ($row = mysqli_fetch_array($result)) {
@@ -78,71 +77,74 @@ $result = mysqli_query($conn, $sql);
                 $itemPrice = $row['price'];
                 $subTotal = $itemPrice * $quantity;
                 $totalPrice += $subTotal;
-
-                $list = $list . '<tr>
-                <td>
-                  <div class="media">
-                    <div class="form-check">
-                      <input class="form-check-input" type="checkbox" value="" checked>
-                    </div>
-                    <div class="d-flex">
-                      <img src="assets/img/gallery/market/' . $itemSchool . '/' . $itemImage . '" alt="" />
-                    </div>
-                    <div class="media-body">
-                      <p>' . "$productName" . '</p>
-                    </div>
-                  </div>
-                </td>
-                <td>
-                  <h5>' . $itemPrice . '원</h5>
-                </td>
-                <td>
-                  <select id="size_select' . $cartId . '" class="form-select" name="size" aria-label="Default select example">';
-
-                $sql = "SELECT * FROM product WHERE item_id = '$itemId' ORDER BY size ";
-                $sizeResult = mysqli_query($conn, $sql);
-
-                while ($sizeRow = mysqli_fetch_array($sizeResult)) {
-                  $list = $list . "<option ";
-                  if ($sizeRow['size'] == $size) {
-                    $list = $list . "selected ";
-                  }
-                  $list = $list . "value={$sizeRow['size']}>{$sizeRow['size']}</option>";
-                }
-
-                $list = $list .
-                  '</select>
-                </td>
-                <td>
-                  <div class="product_count">
-                    <!-- <span class="input-number-decrement"> <i class="ti-minus"></i></span> -->
-                    <input id="quantity_edit' . $cartId . '" class="input-number" type="text" value="' . $quantity . '" min="1" max="20">
-                    <!-- <span class="input-number-increment"> <i class="ti-plus"></i></span> -->
-                  </div>
-                </td>
-                <td>
-                  <h5>' . $subTotal . '원</h5>
-                </td>
-                <td>
-                  <form action="delete_from_cart.php" method="post">
-                    <div>
-                      <input name="cart_id" type="hidden" value="' . $cartId . '">
-                      <input class="genric-btn default" type="submit" value="삭제">
-                    </div>
-                  </form>
-                  <form action="update_cart.php" method="post" onsubmit="changeOnSubmit(' . $cartId . ')">
-                    <div>
-                      <input name="cart_id" type="hidden" value="' . $cartId . '">
-                      <input id="size' . $cartId . '" name="size" type="hidden" value="' . $size . '">
-                      <input id="quantity' . $cartId . '" name="quantity" type="hidden" value="' . $quantity . '">
-                      <input class="genric-btn default" type="submit" value="변경">
-                    </div>
-                  </form>
-                </td>
-              </tr>';
-              }
-              echo $list;
               ?>
+
+                <tr>
+                  <td>
+                    <div class="media">
+                      <div class="form-check">
+                        <input class="form-check-input" type="checkbox" value="" checked>
+                      </div>
+                      <div class="d-flex">
+                        <img src="assets/img/gallery/market/<?= $itemSchool . '/' . $itemImage ?>" alt="" />
+                      </div>
+                      <div class="media-body">
+                        <p><?= "$productName" ?></p>
+                      </div>
+                    </div>
+                  </td>
+                  <td>
+                    <h5><?= $itemPrice ?>원</h5>
+                  </td>
+                  <td>
+                    <select id="size_select<?= $cartId ?>" class="form-select" name="size" aria-label="Default select example">;
+
+                      <?php
+                      $list = '';
+                      $sql = "SELECT * FROM product WHERE item_id = '$itemId' ORDER BY size ";
+                      $sizeResult = mysqli_query($conn, $sql);
+
+                      while ($sizeRow = mysqli_fetch_array($sizeResult)) {
+                        $list = $list . "<option ";
+                        if ($sizeRow['size'] == $size) {
+                          $list = $list . "selected ";
+                        }
+                        $list = $list . "value={$sizeRow['size']}>{$sizeRow['size']}</option>";
+                      }
+
+                      echo $list;
+                      ?>
+
+                    </select>
+                  </td>
+                  <td>
+                    <div class="product_count">
+                      <!-- <span class="input-number-decrement"> <i class="ti-minus"></i></span> -->
+                      <input id="quantity_edit<?= $cartId ?>" class="input-number" type="text" value="<?= $quantity ?>" min="1" max="20">
+                      <!-- <span class="input-number-increment"> <i class="ti-plus"></i></span> -->
+                    </div>
+                  </td>
+                  <td>
+                    <h5><?= $subTotal ?>원</h5>
+                  </td>
+                  <td>
+                    <form action="delete_from_cart.php" method="post">
+                      <div>
+                        <input name="cart_id" type="hidden" value="<?= $cartId ?>">
+                        <input class="genric-btn default" type="submit" value="삭제">
+                      </div>
+                    </form>
+                    <form action="update_cart.php" method="post" onsubmit="changeOnSubmit(<?= $cartId ?>)">
+                      <div>
+                        <input name="cart_id" type="hidden" value="<?= $cartId ?>">
+                        <input id="size<?= $cartId ?>" name="size" type="hidden" value="<?= $size ?>">
+                        <input id="quantity<?= $cartId ?>" name="quantity" type="hidden" value="<?= $quantity ?>">
+                        <input class="genric-btn default" type="submit" value="변경">
+                      </div>
+                    </form>
+                  </td>
+                </tr>
+              <?php } ?>
               <tr>
                 <td></td>
                 <td></td>
@@ -153,14 +155,17 @@ $result = mysqli_query($conn, $sql);
                 </td>
                 <td>
                   <h5><?= $totalPrice ?>원</h5>
-                </td </tr>
+                </td>
+              </tr>
             </tbody>
           </table>
-          <div class="checkout_btn_inner float-right">
-            <a class="btn_1" href="#">선택상품 주문</a>
-            <a class="btn_1" href="#">전체상품 주문</a>
-            <a class="btn_1" href="#">쇼핑 계속하기</a>
-          </div>
+          <form action="checkout.php" method="post">
+            <div class="checkout_btn_inner float-right">
+              <button type="submit" class="btn_1">선택상품 주문</a>
+                <button type="submit" class="btn_1">전체상품 주문</button>
+                <a class="btn_1" href="#">쇼핑 계속하기</a>
+            </div>
+          </form>
         </div>
       </div>
   </section>
